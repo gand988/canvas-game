18d3d542f37b4813459384a99cf616c9443e1373
Matteo 'Gand988' Zanda <gand988@gmail.com>
2019-06-11 18:58:22 +0200

    before trying to destroy the code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /**
- * Project: 
- * create a simple game that will shoot laser and destroy squares that come towards you
- *  
+ * Project: create a simple game that will shoot laser and destroy squares that
+ * come towards you
+ *
  */
 
 /**
- * CURSOR LINE MOVEMENT
- * lineH = horizontal line, width 100% move on Y coords from top-bottom
- * lineV = vertical line, heigh 100% move on X coods from left-right
+ * CURSOR LINE MOVEMENT lineH = horizontal line, width 100% move on Y coords
+ * from top-bottom lineV = vertical line, heigh 100% move on X coods from
+ * left-right
  */
 
 let lineH = document.querySelector('.lineH');
@@ -22,15 +22,19 @@ let ctx = myCanvas.getContext('2d');
 let btnStart = document.querySelector('.start');
 
 /**
- * pageX and pageY is not good if the page scrolling is on. 
- * better use screenX - screenY
- * screen bugs
- * clientX clientY
+ * pageX and pageY is not good if the page scrolling is on. better use screenX -
+ * screenY screen bugs with the position of the cursor and the lines clientX
+ * clientY
  *  */
+let root = document.documentElement;
+root.addEventListener('mousemove', e=>{
+  root.style.setProperty('--mouse-x', e.clientX + 'px');
+  root.style.setProperty('--mouse-y', e.clientY + 'px');
+});
 document.addEventListener('mousemove', e => {
-  lineH.setAttribute("style", "top: " + e.clientY + "px;");
-  lineV.setAttribute("style", "left: " + e.clientX + "px;");
-  coordInfo.innerHTML = `x: ${e.clientX} || y: ${e.clientY}`
+  // lineH.setAttribute("style", "top: " + e.clientY + "px;");
+  // lineV.setAttribute("style", "left: " + e.clientX + "px;");
+  // coordInfo.innerHTML = `x: ${e.clientX} || y: ${e.clientY}`
   if (e.clientX <= canvas.width && e.clientY <= canvas.height) {
     canvasClick(e);
   }
@@ -38,55 +42,31 @@ document.addEventListener('mousemove', e => {
 
 function canvasClick(e) {
   canvas.addEventListener('click', (e) => {
-    coord_canva.innerHTML = `x: ${e.clientX} || y: ${e.clientY}`
+    // coord_canva.innerHTML = `x: ${e.clientX} || y: ${e.clientY}`
   });
 }
 
 /**
- * code for the canvas  
- * 
+ * need to change this variables to be 1440 * 900 canvas.width = '1366';
+ * canvas.height = '768';
+ *
  */
-
-// need to change this variables to be 
-// 1440 * 900
-// canvas.width = '1366';
-// canvas.height = '768';
 canvas.height = '250';
 canvas.width = canvas.height * 1.8;
 let ratioW = 16;
 let ratioH = 9;
 
 /**
- * ratio 16:9 - for every 16px in width the height increese by 9
- * width : 16
- * height : 9
- * X : width / 2
- * Y : height / 2
- * 16/2 = 8
- * 9/2=4.5
- * 8/2=4
- * 4.5/2=2.25
+ * ratio 16:9 - for every 16px in width the height increese by 9 width : 16
+ * height : 9 X : width / 2 Y : height / 2 16/2 = 8 9/2=4.5 8/2=4 4.5/2=2.25
  */
 
 /**
  * there will be square that will show randomly inside the canvas
- * 
+ *
  */
 
-// DRAW A LINE 
-// *****************************************
-
-
-// bottom lines that come close to the player
-/**
- * moving towards the player 
- * moveTo(-x, +y)
- * lineTo(+x, +y)
- * 1) try with 1px (maybe need to calc with the degree of the lines)
- */
-let x1 = 256;
-let x2 = 384;
-let y = 240;
+// speed for the increasing of the rectangles 
 let dratio = 6;
 let innerRectWidth = (canvas.width * 25) / 100;
 let innerRectHeight = (canvas.height * 25) / 100;
@@ -111,7 +91,7 @@ function Rectangle(
   innerRectWidth,
   innerRectHeight,
   innerRectStartX,
-  innerRectStartY, ) {
+  innerRectStartY) {
 
   this.animateInnerRectStartX = animateInnerRectStartX;
   this.animateInnerRectStartY = animateInnerRectStartY;
@@ -124,7 +104,8 @@ function Rectangle(
   this.innerRectHeight = innerRectHeight;
   this.innerRectStartX = innerRectStartX;
   this.innerRectStartY = innerRectStartY;
-  this.draw = function () {
+  // draw the rectangle that will move towards you 
+  this.draw_rekt = function () {
     // MOVEMENT ELEMENT
     ctx.beginPath();
     ctx.strokeStyle = "#10eaf0";
@@ -136,29 +117,26 @@ function Rectangle(
     );
     ctx.stroke();
     // console.log('draw');
-
   }
 
   this.update = function () {
-    // console.log('update');
-    // when the animated width > canvas.width come back to the originals proportions
+    // console.log('update'); when the animated width > canvas.width come back
+    // to the originals proportions
     if (this.animateInnerRectWidth > canvas.width) {
-
       this.animateInnerRectWidth = this.innerRectWidth;
       this.animateInnerRectHeight = this.innerRectHeight;
       this.animateInnerRectStartX = this.innerRectStartX;
       this.animateInnerRectStartY = this.innerRectStartY;
     }
 
-    // dratio == ratio velocity
-    // this.dratio = 6;
+    // dratio == ratio velocity this.dratio = 6;
     this.animateInnerRectWidth += (this.ratioW / this.dratio);
     this.animateInnerRectHeight += (this.ratioH / this.dratio);
     // dratio * 2 because the X & Y need to move in the opposite direction.
     this.animateInnerRectStartX -= (this.ratioW / (this.dratio * 2));
     this.animateInnerRectStartY -= (this.ratioH / (this.dratio * 2));
 
-    this.draw();
+    this.draw_rekt();
   }
 }
 var rekt = new Rectangle(
@@ -174,9 +152,33 @@ var rekt = new Rectangle(
   innerRectStartX,
   innerRectStartY);
 
+let posX = 0;
+let posY = 0;
+canvas.addEventListener('mousemove', e => {
+
+  /**
+   * can't find a good way to track the mouse inside the canvas because there is
+   * always a space for the border + other stuff
+   *
+   */
+    // posX = e.clientX; posY = e.clientY;
+    posX = e.pageX;
+    posY = e.pageY;
+    // console.log(posX, posY)
+});
+
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+  // DRAW A RECTANGLE
+  // *****************************************
+  // c.fillRect(x, y, width, height); 
+  // STATIC ELEMENT
+  ctx.beginPath();
+  ctx.strokeStyle = "#10eaf0";
+  ctx.rect(innerRectStartX, innerRectStartY, innerRectWidth, innerRectHeight);
+  ctx.stroke();
 
   // top-left line 
   ctx.beginPath();
@@ -192,7 +194,7 @@ function animate() {
   ctx.strokeStyle = "#10eaf0";
   ctx.stroke();
 
-  // bottom-right line 
+  // bottom-right
   ctx.beginPath();
   ctx.moveTo(canvas.width, canvas.height);
   ctx.lineTo((innerRectWidth + innerRectStartX), (innerRectHeight + innerRectStartY));
@@ -206,43 +208,31 @@ function animate() {
   ctx.strokeStyle = "#10eaf0";
   ctx.stroke();
 
-  // DRAW A RECTANGLE
-  // *****************************************
-  // c.fillRect(x, y, width, height);
-  // STATIC ELEMENT
+  // cursor
   ctx.beginPath();
-  ctx.strokeStyle = "#10eaf0";
-  ctx.rect(innerRectStartX, innerRectStartY, innerRectWidth, innerRectHeight);
+  ctx.moveTo(posX, posY);
+  ctx.lineTo(posX, 0);
+  ctx.lineTo(posX, canvas.height);
   ctx.stroke();
-  // why dont' reset the thing...
-  // MOVEMENT ELEMENT
-  // ctx.beginPath();
-  // ctx.strokeStyle = "#10eaf0";
-  // ctx.rect(animateInnerRectStartX, animateInnerRectStartY, animateInnerRectWidth, animateInnerRectHeight);
-  // ctx.stroke();
 
-  if (animateInnerRectWidth > canvas.width) {
-    console.log('22');
-
-    // animateInnerRectWidth = innerRectWidth;
-    // animateInnerRectHeight = innerRectHeight;
-    // animateInnerRectStartX = innerRectStartX;
-    // animateInnerRectStartY = innerRectStartY;
-    // rekt.update();
-
-  }
+  ctx.beginPath();
+  ctx.moveTo(posX, posY);
+  ctx.lineTo(0, posY);
+  ctx.lineTo(canvas.width, posY);
+  ctx.strokeStyle = "#10eaf0";
+  ctx.stroke();
 
   /**
-   * create random square
-   * todo: add some random colors (tron related)
+   * create random square todo: add some random colors (tron related)
    */
-  // ctx.beginPath();
-  // ctx.strokeStyle = "#10eaf0";
+  // ctx.beginPath(); ctx.strokeStyle = "#10eaf0";
   // ctx.rect(animateInnerRectStartX + 30, animateInnerRectStartY + 10, 16, 9);
   // ctx.stroke();
+
   rekt.update();
 }
 
-btnStart.addEventListener('click', function () {
+// need a check to see if pressed btnStart.addEventListener('click', function ()
+// {
   animate();
-}) 
+// });
